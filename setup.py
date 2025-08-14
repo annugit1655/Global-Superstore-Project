eb6b9ae43cb57550ec389957c301f879673591bc
ayannu <ayannu1655@gmail.com>
2025-08-14 19:28:07 +0530

    'all'

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='OrderReturnClassifier is a machine learning project that predicts whether an order will be returned or not.',
+    author='ayannu',
+    license='',
+)
